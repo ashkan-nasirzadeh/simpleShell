43853adedf9f00f6bb4b4028e37cceb9861d0346
ashkan-nasirzadeh <ashkan.codecman.01@gmail.com>
2019-11-30 23:15:17 +0330

    long texts display resolved

--- test/shell.js
+++ test/shell.js
@@ -6,6 +6,7 @@ var shellObj = {
     'prompt-answer-color': 'yellow',
     'commands': {
         'hello': ['hi', []],
+        'longtext': ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', ''],
         'how are you': ['im fine thanks', ''],
         'help': ['commandHelp', ''],
         'cls': ['commandCls', ''],
@@ -163,14 +164,14 @@ function showAnswer(answer, container) {
         position: 'relative',
         border: '1px solid black',
         width: '100%',
-        height: '30px',
         boxSizing: 'border-box',
         paddingLeft: '3px',
         display: 'flex',
         justifyContent: 'flex-start',
         alignItems: 'center',
         fontSize: '100',
-        color: shellObj['prompt-answer-color']
+        color: shellObj['prompt-answer-color'],
+        overflowY: 'hidden'
     });
     makeAPrompt(shellObj['shell-container']);
 }
